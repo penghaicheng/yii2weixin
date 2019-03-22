7a5d559675bbee26186ee4442566882c94e492ed
derek <22865900737@qq.com>
2019-03-22 23:22:30 +0800

    add sql

--- common/services/weixin/MessageService.php
+++ common/services/weixin/MessageService.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace app\common\services\weixin;
 
 use app\common\services\BaseService;
@@ -6,9 +7,11 @@ use app\models\market\MarketQrcode;
 use app\models\market\QrcodeScanHistory;
 use app\models\WxHistory;
 
-class MessageService extends BaseService {
+class MessageService extends BaseService
+{
 
-    public static function add($xml ){
+    public static function add($xml)
+    {
 
         $data = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
         $type = trim($data->MsgType);
@@ -16,7 +19,7 @@ class MessageService extends BaseService {
         $to_openid = $data->ToUserName;
         $date_now = date("Y-m-d H:i:s");
 
-        switch($type){
+        switch ($type) {
             case "location":
                 $content = trim($data->Label);
                 break;
@@ -49,25 +52,25 @@ class MessageService extends BaseService {
         $model_wx_history->created_time = $date_now;
         $model_wx_history->save(0);
 
-        if( in_array( $type, [ "event" ] ) && $content == "subscribe" ){
-			$event_key = $data->EventKey;
-			if( $event_key ){
-				$qrcode_key = str_replace( "qrscene_","",$event_key );
-				$qrcode_info = MarketQrcode::findOne( [ 'id' => $qrcode_key ]);
-				if( $qrcode_info ){
-					$qrcode_info->total_scan_count += 1;
-					$qrcode_info->updated_time = date("Y-m-d H:i:s");
-					$qrcode_info->update( 0 );
+        if (in_array($type, ["event"]) && $content == "subscribe") {
+            $event_key = $data->EventKey;
+            if ($event_key) {
+                $qrcode_key = str_replace("qrscene_", "", $event_key);
+                $qrcode_info = MarketQrcode::findOne(['id' => $qrcode_key]);
+                if ($qrcode_info) {
+                    $qrcode_info->total_scan_count += 1;
+                    $qrcode_info->updated_time = date("Y-m-d H:i:s");
+                    $qrcode_info->update(0);
 
-					$model_scan_history = new QrcodeScanHistory();
-					$model_scan_history->openid = $from_openid;
-					$model_scan_history->qrcode_id = $qrcode_info['id'];
-					$model_scan_history->created_time = date("Y-m-d H:i:s");
-					$model_scan_history->save( 0 );
-				}
-			}
+                    $model_scan_history = new QrcodeScanHistory();
+                    $model_scan_history->openid = $from_openid;
+                    $model_scan_history->qrcode_id = $qrcode_info['id'];
+                    $model_scan_history->created_time = date("Y-m-d H:i:s");
+                    $model_scan_history->save(0);
+                }
+            }
         }
     }
 
-    
+
 } 
--- modules/weixin/controllers/JssdkController.php
+++ modules/weixin/controllers/JssdkController.php
@@ -1,24 +1,28 @@
 <?php
+
 namespace app\modules\weixin\controllers;
 
 use app\common\services\weixin\RequestService;
 use Yii;
 
-class JssdkController extends \yii\web\Controller {
+class JssdkController extends \yii\web\Controller
+{
 
-    public function actionIndex(){
+    public function actionIndex()
+    {
         $this->setWeixinConfig();
         return $this->renderJSON($this->getSignPackage());
     }
 
-    private function getSignPackage() {
+    private function getSignPackage()
+    {
         $jsapiTicket = $this->getJsApiTicket();
-        if( $jsapiTicket == 40001){
+        if ($jsapiTicket == 40001) {
             $jsapiTicket = $this->getJsApiTicket(true);
         }
         $url = trim(Yii::$app->request->get("url"));
-        if( !$url ){
-            $url = isset( $_SERVER['HTTP_REFERER'] )?$_SERVER['HTTP_REFERER']:'';
+        if (!$url) {
+            $url = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
         }
 
         $timestamp = time();
@@ -26,17 +30,18 @@ class JssdkController extends \yii\web\Controller {
         $string = "jsapi_ticket=$jsapiTicket&noncestr=$nonceStr&timestamp=$timestamp&url=$url";
         $signature = sha1($string);
         $signPackage = array(
-            "appId"     => RequestService::getAppId(),
-            "nonceStr"  => $nonceStr,
+            "appId" => RequestService::getAppId(),
+            "nonceStr" => $nonceStr,
             "timestamp" => $timestamp,
-            "url"       => $url,
+            "url" => $url,
             "signature" => $signature,
             //"string" => $string
         );
         return $signPackage;
     }
 
-    private function createNonceStr($length = 16) {
+    private function createNonceStr($length = 16)
+    {
         $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         $str = "";
         for ($i = 0; $i < $length; $i++) {
@@ -46,42 +51,45 @@ class JssdkController extends \yii\web\Controller {
     }
 
 
-    private function getJsApiTicket( $force = false) {
-        $cache_key = 'wx_js_tkt_'.substr( RequestService::getAppId() ,2);
-		$cache = Yii::$app->cache;
-		$ticket = $cache->get($cache_key);
+    private function getJsApiTicket($force = false)
+    {
+        $cache_key = 'wx_js_tkt_' . substr(RequestService::getAppId(), 2);
+        $cache = Yii::$app->cache;
+        $ticket = $cache->get($cache_key);
 
-        if ( !$ticket || $force ) {
+        if (!$ticket || $force) {
             $accessToken = RequestService::getAccessToken();
             $url = "ticket/getticket?type=jsapi&access_token=$accessToken";
             $res = RequestService::send($url);
             $ticket_info = $res;
-            if( isset($ticket_info['errcode'])  && $ticket_info['errcode'] != 0 ){
+            if (isset($ticket_info['errcode']) && $ticket_info['errcode'] != 0) {
                 return $ticket_info['errcode'];
             }
 
-            $ticket = isset( $ticket_info['ticket'] )? $ticket_info['ticket']:'';
+            $ticket = isset($ticket_info['ticket']) ? $ticket_info['ticket'] : '';
 
             if ($ticket) {
-				$cache->set($cache_key,$ticket,$ticket_info['expires_in'] - 200 );
+                $cache->set($cache_key, $ticket, $ticket_info['expires_in'] - 200);
             }
 
         }
         return $ticket;
     }
 
-    private function setWeixinConfig(){
-		$config = \Yii::$app->params['weixin'];
-		RequestService::setConfig( $config['appid'],$config['token'],$config['sk'] );
+    private function setWeixinConfig()
+    {
+        $config = \Yii::$app->params['weixin'];
+        RequestService::setConfig($config['appid'], $config['token'], $config['sk']);
     }
 
-    protected function renderJSON($data=[], $msg ="ok", $code = 200){
+    protected function renderJSON($data = [], $msg = "ok", $code = 200)
+    {
         header('Content-type: application/json');
         echo json_encode([
             "code" => $code,
-            "msg"   =>  $msg,
-            "data"  =>  $data,
-            "req_id" =>  uniqid(),
+            "msg" => $msg,
+            "data" => $data,
+            "req_id" => uniqid(),
         ]);
         return Yii::$app->end();
     }
